90ea94469446d10a9362325ecebbbbeeedc50eb7
Frank Barros <fd.barros@yahoo.com>
2024-06-23 16:38:51 +0800

    Add image to hero, update mobile responsiveness

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import Link from "next/link";
+import { LuConstruction } from "react-icons/lu";
 
 interface ProjectCardProps {
   projectData: {
@@ -8,6 +9,7 @@ interface ProjectCardProps {
     link: string;
     image: string;
     Icon: React.ComponentType;
+    underConstruction?: boolean;
   };
 }
 
@@ -24,19 +26,38 @@ const IconWrapper: React.FC<{ Icon: React.ComponentType<IconProps> }> = ({
   </div>
 );
 
+const UnderConstructionWrapper: React.FC<{
+  Icon: React.ComponentType<IconProps>;
+}> = ({ Icon }) => (
+  <div className="absolute inset-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+    <Icon size={48} />
+  </div>
+);
+
 export default function ProjectCard({ projectData }: ProjectCardProps) {
-  const { title, description, image, Icon } = projectData;
+  const { title, description, image, Icon, underConstruction, link } =
+    projectData;
   return (
-    <div className="group flex flex-col p-8 min-w-[260px] sm:min-w-[300px] max-w-[300px]">
-      <div className="relative rounded-lg bg-muted h-44 w-60 md:h-52 md:w-64 cursor-pointer overflow-hidden">
-        <img
-          src={image}
-          alt="project image"
-          className="absolute inset-0 w-full h-full object-cover opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-        />
-        <IconWrapper Icon={Icon} />
-      </div>
-      <div className="flex flex-col mt-3 text-muted-foreground">
+    <div className="group flex flex-col mx-auto p-4 sm:p-8 min-w-[260px] sm:min-w-[300px] max-w-[300px]">
+      {!underConstruction ? (
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <div className="relative rounded-lg bg-muted h-44 w-60 md:h-52 md:w-64 cursor-pointer overflow-hidden mx-auto">
+            <img
+              src={image}
+              alt="project image"
+              className="absolute inset-0 w-full h-full object-cover opacity-0 
+            group-hover:opacity-100 transition-opacity duration-300"
+            />
+            <IconWrapper Icon={Icon} />
+          </div>
+        </a>
+      ) : (
+        <div className="relative rounded-lg bg-muted h-44 w-60 md:h-52 md:w-64 cursor-normal overflow-hidden mx-auto">
+          <UnderConstructionWrapper Icon={LuConstruction} />
+          <IconWrapper Icon={Icon} />
+        </div>
+      )}
+      <div className="flex flex-col mt-3 text-foreground w-60 mx-auto">
         <div className="text-sm font-semibold">{title}</div>
         <div className="text-sm">{description}</div>
       </div>
